787171aec3986b0f1a147596651f6bb0d5a20f49
Kiran Shrestha <kdstha10@gmail.com>
2024-06-15 06:14:29 -0400

    Updated with sportbugs report

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 
 /**
  * Class for handling some game logic for hangman game.
- * Every game starts with a score of 10 and the points are reduced based on the description of "makeGuess". Points holds the current score for one game.
- * Game is lost when the user made 10 guesses and did not guess the word.
+ * Every game starts with a score of 10 and the points are reduced based on the description of "makeGuess".
+ * Points holds the current score for one game. Game is lost when the user made 10 guesses and did not guess the word.
  *
  */
 public class Game {
@@ -12,8 +12,8 @@ public class Game {
     /** Holds the points for the game. */
     public int points;
 
-    /** Holds the round of the game. */
-    int round;
+//    /** Holds the round of the game. */
+//    int round;
 
     /** Holds the player name for the game. */
     String name;
@@ -21,46 +21,53 @@ public class Game {
     /** Holds the answer for the current game. */
     String answer;
 
+//    /** The path to the file holding the leaderboard. */
+//    private String leaderboard = "leaderboard.txt";
 
-    /** The path to the file holding the leaderboard.*/
-    private String leaderboard = "leaderboard.txt";
-
-    /** The status of the game. {0 - In progress, 1 - Game won, 2 - game over}*/
+    /** The status of the game. {0 - In progress, 1 - Game won, 2 - game over} */
     protected int gameStatus = 0;
 
     // all things that were already guessed, needs to be cleared for each game
-    ArrayList<String> guesses = new ArrayList<String>();
+    ArrayList<String> guesses = new ArrayList<>();
 
     // all answers from makeGuess that were already returned
-    ArrayList<Double> answers = new ArrayList<Double>();
+    ArrayList<Double> answers = new ArrayList<>();
 
     /**
      * Gets the name for the game.
      * @return String The name.
      */
-    public String getName() { return this.name; }
+    public String getName() {
+        return this.name;
+    }
 
     /**
      * Gets the answer for the game.
      * @return String The Answer.
      */
-    public String getAnswer() { return this.answer.toLowerCase(); }
+    public String getAnswer() {
+        return this.answer.toLowerCase();
+    }
 
     /**
      * Gets the current status of the game.
-     * @return int
+     * @return int The current game status.
      */
-    public int getGameStatus() { return this.gameStatus; }
+    public int getGameStatus() {
+        return this.gameStatus;
+    }
 
     /**
      * Sets the score for the game.
-     * @param points
+     * @param points The points to set.
      */
-    public void setPoints(int points) { this.points = points; }
+    public void setPoints(int points) {
+        this.points = points;
+    }
 
     /**
      * Gets the score for the game.
-     * @return int
+     * @return int The current points.
      */
     public int getPoints() {
         return this.points;
@@ -69,12 +76,12 @@ public class Game {
     /**
      * Counts the number of letters that have been guessed correctly during the game.
      * The number of correct letter guesses will be returned in result.
-     * @return int
+     * @return int The number of correct letters.
      */
     public int countCorrectLetters() {
         int result = 0;
         if (!guesses.isEmpty()) {
-            for(int i = 0; i < this.answer.length(); i++) {
+            for (int i = 0; i < this.answer.length(); i++) {
                 String current = String.valueOf(this.answer.charAt(i));
                 if (guesses.contains(current)) {
                     result++;
@@ -85,14 +92,14 @@ public class Game {
     }
 
     /**
-     * Counts how often a letter occurs
-     * @param letter char
-     * @return int
+     * Counts how often a letter occurs.
+     * @param letter The letter to count.
+     * @return int The count of the letter.
      */
     public int countLetters(char letter) {
         int count = 0;
         int i = 0;
-        while(this.getAnswer().indexOf(letter, i) >= 0){
+        while (this.getAnswer().indexOf(letter, i) >= 0) {
             i = this.getAnswer().indexOf(letter, i) + 1;
             count++;
         }
@@ -101,9 +108,9 @@ public class Game {
 
     /**
      * Constructs a new game with a random word.
-     * @param name
+     * @param name The name of the player.
      */
-    public Game(String name){
+    public Game(String name) {
         this.name = name;
         setRandomWord();
         setPoints(5);
@@ -111,27 +118,30 @@ public class Game {
 
     /**
      * Constructs a new game with a given word and given name.
-     * @param name
+     * @param fixedWord The fixed word for the game.
+     * @param name The name of the player.
      */
-    public Game(String fixedWord, String name){
+    public Game(String fixedWord, String name) {
         this.name = name;
         this.answer = fixedWord;
         setPoints(10);
     }
 
     /**
-     * Constructs a new game with no arguments, empty name and answer
+     * Constructs a new game with no arguments, empty name and answer.
      */
-    public Game(){
+    public Game() {
         this.name = "";
         this.answer = "";
         setPoints(10);
     }
 
     /**
-     * Sets the name and answers of an already existing game and clears the guesses
+     * Sets the name and answers of an already existing game and clears the guesses.
+     * @param answer The answer to set.
+     * @param name The name of the player.
      */
-    public void initGame(String answer, String name){
+    public void initGame(String answer, String name) {
         this.name = name;
         this.answer = answer;
         this.gameStatus = 0;
@@ -142,7 +152,7 @@ public class Game {
 
     /**
      * Checks if the guess made is correct, should ignore upper/lower case. Should give points based on made guess.
-     * Method returns a double, number of the double has different meanings
+     * Method returns a double, number of the double has different meanings:
      * 0 Correct guess
      * 1.x Letter is in the word, x represents the number of times the letter is in the word
      * 2.0 Guess has correct length
@@ -154,29 +164,8 @@ public class Game {
      * 5. After 10 guesses the game ends and is set to game over
      * 5.1 If the player keeps guessing even though the status is not InProgress
      *
-     * The returned answer and the guess needs to be added to the respective lists for tracking.
-     *
-     * If letter:
-     *  Return 1.NumOfOccurrence, 1.0 for the letter not being in the word, 1.1 for being in there once etc.
-     *  Add points according to NumOfOccurance
-     *
-     * If word (go by this order of checks):
-     *  If word is correct return 0.0 and add points based on the length of the word (e.g. dog - 3 points, horse - 5 points), set game status to won
-     *  If word is incorrect but has correct length return 2.0 and add 1 point
-     *  If word is incorrect and is partially included in the word return 3.0 and add 2 points
-     *  If word is too long or too short return 2.1, 2.2 accordingly and reduce points based on how off the word is (e.g. how many letters off)
-     *
-     * For either:
-     *  Guess was already used, reduce points by 2 and return 4.0 (checked before 4.1 error). Guess still counts toward made guesses.
-     *  Guess includes numbers/symbols etc. (so more than just letters) reduce points by 3 and return 4.1, the guess is still added to the list of guesses
-     *
-     *
-     * Score can also be negative, that is no problem.
-     * When the player guessed 10 times and did not guess the word set the game to game over (status) and return 5.0 (no matter if there was another error).
-     * If the player guesses again, even though game status is won or game over return 5.1.
-     *
-     * @param guess
-     * @return double returns the appropriate number
+     * @param guess The guess made by the player.
+     * @return double The result of the guess.
      */
     public double makeGuess(String guess) {
         if (gameStatus != 0) {
@@ -244,15 +233,13 @@ public class Game {
         return 2.0;
     }
 
-
     /**
-     * Pulls out a random animal and sets it as answer
+     * Pulls out a random animal and sets it as answer.
      */
     public void setRandomWord() {
-        String[] animals = {"dog", "horse", "pony", "cat", "lion", "bear","lioncub"};
+        String[] animals = {"dog", "horse", "pony", "cat", "lion", "bear", "lioncub"};
 
-        int randomNum = 0;
-        randomNum = (int) (Math.floor(Math.random() * (100 - 2 + 1) + 2) % animals.length);
+        int randomNum = (int) (Math.floor(Math.random() * (100 - 2 + 1) + 2) % animals.length);
         this.answer = animals[randomNum];
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
     public static void main(String[] args) {
-        // just some calls
+        // Just some calls
         System.out.println("Getting started");
         Game game = new Game("Student");
-        System.out.println("Current word: " + game.answer);
+        System.out.println("Current word: " + game.getAnswer());
         System.out.println(game.makeGuess("a"));
         System.out.println("Automatic guess a");
-
 
         // Rough game play
         Game newgame = new Game("Dr. M.");
@@ -17,8 +17,8 @@ public class Main {
         while (newgame.getGameStatus() == 0) {
             String message = scanner.nextLine();
             System.out.println(newgame.makeGuess(message));
-            System.out.println("Score: " + newgame.points);
-            newgame.countCorrectLetters();
+            System.out.println("Score: " + newgame.getPoints());
+            int correctLetters = newgame.countCorrectLetters();  // SER316 TASK 2 SPOTBUGS FIX
         }
     }
 }
